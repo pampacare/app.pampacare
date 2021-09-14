d7a6093f0b43fb907c42d2f8f62eefa3fec75e73
Brandow Buenos <brandowbuenos@gmail.com>
2021-09-14 13:19:12 -0300

    refactor: change textField color

--- lib/app/shared/theme/app_colors.dart
+++ lib/app/shared/theme/app_colors.dart
@@ -7,7 +7,7 @@ class AppColors {
   static const Color blue = Color(0xFF94B5FF);
   static const Color white = Color(0xFFFFFFFF);
   static const Color grey10 = Color(0xFFF5F5F5);
-  static const Color textField = Color(0xFFF8F8F8);
+  static const Color textField = Color(0xFFF3F3F3);
   static const Color black = Color(0xFF000000);
   static const Color text = Color(0xFF455A64);
   static const Color hintText = Color(0xFF757575);
