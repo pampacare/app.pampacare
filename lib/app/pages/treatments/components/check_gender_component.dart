b8ba43ffc287ea340c0cd199598879da957b391c
Thalis Silva <thalis.silva@zallpy.com>
2021-09-28 18:21:22 -0300

    feat: implementation tratmeant layout page

---
+++ lib/app/pages/treatments/components/check_gender_component.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:pampacare/app/shared/theme/app_colors.dart';
+
+class CheckGenderComponent extends StatelessWidget {
+  final String title;
+  final bool value;
+  final void Function() onChanged;
+  const CheckGenderComponent({
+    Key? key,
+    required this.title,
+    required this.value,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(value
+            ? Icons.radio_button_checked_outlined
+            : Icons.radio_button_unchecked_outlined),
+        SizedBox(width: 10),
+        Text(
+          title,
+          style: TextStyle(fontSize: 16, color: AppColors.hintText),
+        )
+      ],
+    );
+  }
+}
--- lib/app/pages/treatments/treatments_page.dart
+++ lib/app/pages/treatments/treatments_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:pampacare/app/pages/components/add_button.dart';
-import 'package:pampacare/app/pages/components/option_button.dart';
+import 'package:pampacare/app/pages/components/default_button.dart';
+import 'package:pampacare/app/pages/components/default_text_field.dart';
 import 'package:pampacare/app/pages/components/title_subtitle_component.dart';
-import 'package:pampacare/app/shared/theme/app_icons.dart';
-import 'package:pampacare/app/shared/theme/app_images.dart';
+import 'package:pampacare/app/pages/treatments/components/check_gender_component.dart';
+import 'package:pampacare/app/shared/utils/screen_size.dart';
 
 class TreatmentsPage extends StatefulWidget {
   const TreatmentsPage({Key? key}) : super(key: key);
@@ -15,40 +15,46 @@ class TreatmentsPage extends StatefulWidget {
 class _TreatmentsPageState extends State<TreatmentsPage> {
   @override
   Widget build(BuildContext context) {
-    return 
-    Scaffold(
-    
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ListView(
-          shrinkWrap: false,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TitleSubtitleComponent(
-                    title: 'Quase lá, quais', subtitle: 'são os tratamentos ?'),
-                    SizedBox(height: 70,),                
-                Column(
-                  children: [
-                    TitleSubtitleComponent(
-                    title: ' ', subtitle: 'Testes realizados'),
-                    SizedBox(height: 70,),        
-               
-
+    return Scaffold(
+      body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: SizedBox(
+            height: ScreenSize.screenHeightSize(context),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TitleSubtitleComponent(
+                      title: "Quase lá, quais",
+                      subtitle: "são os tratamentos?"),
+                  SizedBox(
+                    height: 32,
+                  ),
                  
-                  ],
-                ),
-                 Image.asset(
-                AppImages.dog,
-                fit: BoxFit.contain,
-              ),
-               
-              ],
-            ),
-          ],
-        ),
-      ),
+                  SizedBox(
+                    height: 64,
+                  ),
+                  
+                   TitleSubtitleComponent(
+                      title: "",
+                      subtitle: "Testes realizados"),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      CheckGenderComponent(
+                          title: "ELISA", value: true, onChanged: () {}),
+                      CheckGenderComponent(
+                          title: "TR DPP", value: false, onChanged: () {}),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  DefaultButton(onPressed: () {}, title: "Concluir")
+                ]),
+          )),
     );
   }
 }
